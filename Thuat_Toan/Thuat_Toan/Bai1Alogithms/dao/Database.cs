6587e74a089f857c2c558ee59710eef804755728
anhtrantrung <anh.trantrung.tkh@gmail.com>
2024-05-07 16:26:53 +0700

    Algorithms_B1

---
+++ Thuat_Toan/Thuat_Toan/Bai1Alogithms/dao/Database.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Thuat_Toan.Bai1Alogithms.entity;
+
+namespace Thuat_Toan.Bai1Alogithms
+{
+    internal class Database
+    {
+        public string GetDataByName()
+        {
+            Console.WriteLine("Please enter data");
+            var productNameToFind = Console.ReadLine();
+            return productNameToFind;
+        }
+
+        public int GetDataByCategoryId()
+        {
+            Console.WriteLine("Please enter data");
+            var productNameToFind = Convert.ToInt32(Console.ReadLine());
+            return productNameToFind;
+        }
+
+        public double GetDataByPrice()
+        {
+            Console.WriteLine("Please enter data");
+            var productNameToFind = Double.Parse(Console.ReadLine());
+            return productNameToFind;
+        }
+
+        public Product FindProductByName(List<Product> productList, string nameProduct)
+        {
+            foreach (Product product in productList)
+            {
+                if (product.Name == nameProduct)
+                {
+                    return product;
+                }
+            }
+            return null;
+        }
+
+        public Product FindProductByCategoryId(List<Product> productList, int categoryId)
+        {
+            foreach (Product product in productList)
+            {
+                if (product.CategoryId == categoryId)
+                {
+                    return product;
+                }
+            }
+            return null;
+        }
+
+        public Product FindProductByPrice(List<Product> productList, double price)
+        {
+            foreach (Product product in productList)
+            {
+                if (product.Price == price)
+                {
+                    return product;
+                }
+            }
+            return null;
+        }
+    }
+
+}
---
+++ Thuat_Toan/Thuat_Toan/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using static Thuat_Toan.Bai1Alogithms.Database;
+using Thuat_Toan.Bai1Alogithms;
+using Thuat_Toan.Bai1Alogithms.entity;
+
+namespace Thuat_Toan
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Database database = new Database();
+            List<Product> productList = new List<Product>();
+            productList.Add(new Product("CPU", 750, 10, 1));
+            productList.Add(new Product("GPU", 1000, 5, 2));
+            productList.Add(new Product("RAM", 150, 20, 3));
+
+            //var productNameToFind = database.GetDataByName();
+            //Product foundProduct = database.FindProductByName(productList, productNameToFind);
+
+            var productNameToFind = database.GetDataByCategoryId();
+            Product foundProduct = database.FindProductByCategoryId(productList, productNameToFind);
+
+            //var productNameToFind = database.GetDataByPrice();
+            //Product foundProduct = database.FindProductByPrice(productList, productNameToFind);
+
+            if (foundProduct != null)
+            {
+                Console.WriteLine(foundProduct.Name+ " " + foundProduct.Price + " " + foundProduct.Quality + " " + foundProduct.CategoryId);
+            }
+            else
+            {
+                Console.WriteLine("No data found");
+            }
+        }
+
+
+    }
+}
