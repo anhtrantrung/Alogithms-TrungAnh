dbf55a46c8fb9dc4a7e21064b0144d22293fbc75
anhtrantrung <anh.trantrung.tkh@gmail.com>
2024-05-08 17:18:56 +0700

    Alogihms_B2

--- Thuat_Toan/Thuat_Toan/Bai1Alogithms/dao/Database.cs
+++ Thuat_Toan/Thuat_Toan/Bai1Alogithms/dao/Database.cs
@@ -54,17 +54,197 @@ namespace Thuat_Toan.Bai1Alogithms
             return null;
         }
 
-        public Product FindProductByPrice(List<Product> productList, double price)
+        public List<string> FindProductByPrice(List<Product> productList, double price)
         {
+            List<string> productsWithinPrice = new List<string>();
             foreach (Product product in productList)
             {
-                if (product.Price == price)
+                if (product.Price <= price)
                 {
-                    return product;
+                    productsWithinPrice.Add(product.Name);
                 }
             }
-            return null;
+            return productsWithinPrice;
+        }
+
+        public List<Product> SortByPrice(List<Product> productList)
+        {
+            int n = productList.Count;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (productList[j].Price > productList[j + 1].Price)
+                    {
+                        Product temp = productList[j];
+                        productList[j] = productList[j + 1];
+                        productList[j + 1] = temp;
+                    }
+                }
+            }
+            return productList;
+        }
+
+        public List<Product> SortByName(List<Product> productList)
+        {
+            for (int i = 1; i < productList.Count; i++)
+            {
+                Product key = productList[i];
+                int j = i - 1;
+
+                while (j >= 0 && productList[j].Name.Length < key.Name.Length)
+                {
+                    productList[j + 1] = productList[j];
+                    j = j - 1;
+                }
+                productList[j + 1] = key;
+            }
+            return productList;
+        }
+
+        public List<Product> SortByCategoryName(List<Product> productList, List<string> categoryList)
+        {
+            for (int i = 1; i < productList.Count; i++)
+            {
+                Product key = productList[i];
+                int j = i - 1;
+
+                while (j >= 0 && GetCategoryIndex(productList[j].Name, categoryList) > GetCategoryIndex(key.Name, categoryList))
+                {
+                    productList[j + 1] = productList[j];
+                    j = j - 1;
+                }
+                productList[j + 1] = key;
+            }
+            return productList;
+        }
+
+        public static int GetCategoryIndex(string categoryName, List<string> categoryList)
+        {
+            return categoryList.IndexOf(categoryName);
+        }
+
+        public Dictionary<int, string> CreateMapCategory(List<string> categoryList)
+        {
+            Dictionary<int, string> mapCategory = new Dictionary<int, string>();
+
+            for (int i = 0; i < categoryList.Count; i++)
+            {
+                mapCategory[i + 1] = categoryList[i]; 
+            }
+            return mapCategory;
+        }
+
+        public List<(Product, string)> MapProductByCategory(List<Product> productList, List<string> categoryList)
+        {
+            Dictionary<int, string> mapCategory = CreateMapCategory(categoryList);
+
+            List<(Product, string)> mappedProducts = new List<(Product, string)>();
+            foreach (Product product in productList)
+            {
+                string categoryName = mapCategory.ContainsKey(product.CategoryId) ? mapCategory[product.CategoryId] : "Unknown";
+                mappedProducts.Add((product, categoryName));
+            }
+            return mappedProducts;
+        }
+
+        public Product MinByPrice(List<Product> productList)
+        {
+            if (productList.Count == 0)
+            {
+                return null;
+            }
+
+            Product minProduct = productList[0];
+            foreach (Product product in productList)
+            {
+                if (product.Price < minProduct.Price)
+                {
+                    minProduct = product;
+                }
+            }
+            return minProduct;
+        }
+
+        public Product MaxByPrice(List<Product> productList)
+        {
+            if (productList.Count == 0)
+            {
+                return null; 
+            }
+
+            Product maxProduct = productList[0]; 
+            foreach (Product product in productList)
+            {
+                if (product.Price > maxProduct.Price)
+                {
+                    maxProduct = product; 
+                }
+            }
+            return maxProduct;
+        }
+
+            public double CalculateSalaryRecursive(double salary, int n)
+        {
+            if (n == 1)
+            {
+                return salary;
+            }
+            else
+            {
+                return CalculateSalaryRecursive(salary * 1.1, n - 1);
+            }
+        }
+
+        public double CalculateSalary(double salary, int n)
+        {
+            for (int i = 1; i < n; i++)
+            {
+                salary *= 1.1;     
+            }
+            return salary;
+        }
+
+        public int CalMonthNoRecursive(double money, double rate)
+        {
+            double target = money * 2; 
+            int months = 0;
+
+            while (money < target)
+            {
+                money += money * rate / 100; 
+                months++; 
+            }
+
+            return months;
+        }
+
+        public int CalMonthRecursive(double money, double rate, double target)
+        {
+            if (money >= target)
+            {
+                return 0; 
+            }
+            else
+            {
+                double total = money + money * rate / 100;
+                return 1 + CalMonthRecursive(total, rate, target);
+            }
+        }
+
+        public int CalMonth(double money, double rate)
+        {
+            double target = money * 2;
+            return CalMonthRecursive(money, rate, target);
+        }
+
+        public void PrintMenu(List<MenuItem> menus, string prefix = "")
+        {
+            foreach (var menu in menus)
+            {
+                Console.WriteLine($"{prefix}{menu.Name}");
+                PrintMenu(menu.SubMenuItems, prefix + "--");
+            }
         }
     }
-
 }
--- Thuat_Toan/Thuat_Toan/Program.cs
+++ Thuat_Toan/Thuat_Toan/Program.cs
@@ -13,23 +13,53 @@ namespace Thuat_Toan
 {
     internal class Program
     {
+        private List<Product> productList;
+        Database database = new Database();
+        Bai24Queu<int> bai24Queu = new Bai24Queu<int>();
+        Bai25Stack<int> Bai25Stack = new Bai25Stack<int>();
 
+        public Program()
+        {
+            productList = new List<Product>();
+            productList.Add(new Product("CPU", 750, 10, 1));
+            productList.Add(new Product("RAM", 50, 2, 2));
+            productList.Add(new Product("HDD", 70, 1, 2));
+            productList.Add(new Product("Main", 400, 3, 1));
+            productList.Add(new Product("Keyboard", 30, 8, 4));
+            productList.Add(new Product("Mouse", 25, 50, 4));
+            productList.Add(new Product("VGA", 60, 35, 3));
+            productList.Add(new Product("Monitor", 120, 28, 2));
+            productList.Add(new Product("Case", 120, 28, 5));
+        }
+
+       
         static void Main(string[] args)
         {
-            Database database = new Database();
-            List<Product> productList = new List<Product>();
-            productList.Add(new Product("CPU", 750, 10, 1));
-            productList.Add(new Product("GPU", 1000, 5, 2));
-            productList.Add(new Product("RAM", 150, 20, 3));
+            Program program = new Program();
 
-            //var productNameToFind = database.GetDataByName();
-            //Product foundProduct = database.FindProductByName(productList, productNameToFind);
+            //program.CheckOutputData();
+            //program.TestB6();
+            //program.TestB11();
+            //program.TestB12();
+            //program.TestB13();
+            //program.TestB14();
+            //program.TestB15();
+            //program.TestB16();
+            //program.TestB21();
+            //program.TestB22();
+            program.TestB23();
+            //program.TestB24();
+            //program.TestB25();
 
-            var productNameToFind = database.GetDataByCategoryId();
-            Product foundProduct = database.FindProductByCategoryId(productList, productNameToFind);
 
-            //var productNameToFind = database.GetDataByPrice();
-            //Product foundProduct = database.FindProductByPrice(productList, productNameToFind);
+        }
+        public void TestB4()
+        {
+            var productNameToFind = database.GetDataByName();
+            Product foundProduct = database.FindProductByName(productList, productNameToFind);
+
+            //var productNameToFind = database.GetDataByCategoryId();
+            //Product foundProduct = database.FindProductByCategoryId(productList, productNameToFind);
 
             if (foundProduct != null)
             {
@@ -41,6 +71,159 @@ namespace Thuat_Toan
             }
         }
 
+        public void TestB6()
+        {
+            var priceToFind = database.GetDataByPrice();
+            List<string> productsWithinPrice = database.FindProductByPrice(productList, priceToFind);
+            Console.WriteLine("Products with a price less than or equal to {priceToFind}:");
+            foreach (string productName in productsWithinPrice)
+            {
+                Console.WriteLine(productName);
+            }
+        }
 
+        public void TestB11()
+        {
+            productList = database.SortByPrice(productList);
+            Console.WriteLine("List of products sorted by price from low to high:");
+            foreach (Product product in productList)
+            {
+                Console.WriteLine($"Name: {product.Name}, Price: {product.Price}, Quality: {product.Quality}, ICategoryId: {product.CategoryId}");
+            }
+        }
+
+        public void TestB12()
+        {
+            productList = database.SortByName(productList);
+            Console.WriteLine("Product list after sorting by name length from highest to lowest:");
+            foreach (Product product in productList)
+            {
+                Console.WriteLine($"Name: {product.Name}, Price: {product.Price}, Quality: {product.Quality}, ICategoryId: {product.CategoryId}");
+            }
+        }
+
+        public void TestB13()
+        {
+            List<string> categoryList = new List<string> { "CPU", "Case", "HDD", "Keyboard", "Main", "Monitor", "Mouse", "RAM", "VGA" };
+            productList = database.SortByCategoryName(productList, categoryList);
+            Console.WriteLine("Product list after sorting by category name alphabetically:");
+            foreach (Product product in productList)
+            {
+                Console.WriteLine($"Name: {product.Name}, Price: {product.Price}, Quality: {product.Quality}, ICategoryId: {product.CategoryId}");
+            }
+        }
+
+        public void TestB14()
+        {
+            List<string> categoryList = new List<string> { "Comuter", "Memory", "Card", "Acsesory", "Null" };
+            List<(Product, string)> mappedProducts = database.MapProductByCategory(productList, categoryList);
+            Console.WriteLine("List of products and corresponding categories:");
+            foreach ((Product product, string categoryName) in mappedProducts)
+            {            
+                Console.WriteLine($"Name: {product.Name}, Price: {product.Price}, Quality: {product.Quality}, ICategoryId: {product.CategoryId}, category: {categoryName}");
+            }
+        }
+
+        public void TestB15()
+        {
+            Product minProduct = database.MinByPrice(productList);
+            if (minProduct != null)
+            {
+                Console.WriteLine($"The product with the smallest price is: {minProduct.Name}, Price: {minProduct.Price}");
+            }
+            else
+            {
+                Console.WriteLine("There are no products in the list.");
+            }
+        }
+
+        public void TestB16()
+        {
+            Product maxProduct = database.MaxByPrice(productList);
+            if (maxProduct != null)
+            {
+                Console.WriteLine($"The product with the highest price is: {maxProduct.Name}, Price: {maxProduct.Price}");
+            }
+            else
+            {
+                Console.WriteLine("There are no products in the list.");
+            }
+        }
+
+        public void TestB21()
+        {
+            double initialSalary = 1000;
+            int year = 5;
+
+            //double salary = database.CalculateSalaryRecursive(initialSalary, year);
+            //Console.WriteLine($"Fifth salary {year} is: {salary}");
+
+            double salary = database.CalculateSalary(initialSalary, year);
+            Console.WriteLine($"Fifth salary {year} is: {salary}");
+        }
+
+        public void TestB22()
+        {
+            double money = 1000;
+            double rate = 5;
+            int months = database.CalMonth(money, rate);
+
+            Console.WriteLine($"The number of months it takes for the principal and interest to double is: {months}");
+
+            //int months = database.CalMonthNoRecursive(money, rate);
+            //Console.WriteLine($"The number of months it takes for the principal and interest to double is: {months}");
+        }
+
+        public void TestB23()
+        {
+            MenuItem menu1 = new MenuItem("The Thao");
+            MenuItem ttTrongNuoc = new MenuItem("The thaao trong nuoc");
+            MenuItem ttNuocNgoai = new MenuItem("The thaao nuoc ngoai");
+            menu1.AddSubMenu(ttTrongNuoc);
+            menu1.AddSubMenu(ttNuocNgoai);
+
+            MenuItem menu2 = new MenuItem("Xa Hoi");
+            MenuItem giaoThong = new MenuItem("Giao thong");
+            giaoThong.AddSubMenu(new MenuItem("Giao thong un tac"));          
+            menu2.AddSubMenu(giaoThong);
+
+            List<MenuItem> menus = new List<MenuItem> { menu1, menu2 };
+
+            Console.WriteLine("Menu:");
+            database.PrintMenu(menus);
+        }
+
+        public void TestB24()
+        {
+            bai24Queu.Push(1);
+            bai24Queu.Push(2);
+            bai24Queu.Push(3);
+
+            Console.WriteLine("Prints the elements in first in, first out order");
+            Console.WriteLine(bai24Queu.Get()); 
+            Console.WriteLine(bai24Queu.Get()); 
+            Console.WriteLine(bai24Queu.Get()); 
+
+            Console.WriteLine("Add a new element and retrieve it");
+            bai24Queu.Push(4);
+            Console.WriteLine(bai24Queu.Get());
+
+        }
+
+        public void TestB25()
+        {
+            Bai25Stack.Push(1);
+            Bai25Stack.Push(2);
+            Bai25Stack.Push(3);
+
+            Console.WriteLine("Prints the elements in last in, first out order");
+            Console.WriteLine(Bai25Stack.Get()); 
+            Console.WriteLine(Bai25Stack.Get());
+            Console.WriteLine(Bai25Stack.Get()); 
+
+            Console.WriteLine("Add a new element and retrieve it");
+            Bai25Stack.Push(4);
+            Console.WriteLine(Bai25Stack.Get()); 
+        }
     }
 }
